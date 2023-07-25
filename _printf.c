d2e20598d7971882edb72fe021dd8a7ebf15d630
AnyumbaKE <esanyumba@gmail.com>
2023-07-25 05:25:50 -0700

    printf.c

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _printf - a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	convert_match m[] = {
+	{"%s", printf_string}, {"%c", printf_char},
+	{"%%", printf_percentage}, {"%i", printf_int},
+	{"%d", printf_dec}, {"%r", printf_srev},
+	{"%R", printf_rot13},
+	{"%b", printf_bin}, {"%u", printf_unsigned},
+	{"%o", printf_oct},
+	{"%x", printf_hex}, {"%X", printf_HEX},
+	{"%S", printf_exclusive_string},
+	{"%p", printf_pointer}
+	};
+
+	va_list args;
+	int x = 0, y, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	while (format[x] != '\0')
+	{
+		y = 13;
+		while (y >= 0)
+		{
+			if (m[y].id[0] == format[x] && m[y].id[1] == format[x + 1])
+			{
+				len += m[y].f(args);
+				x = x + 2;
+			}
+			y--;
+		}
+		_putchar(format[x]);
+		len++;
+		x++;
+	}
+	va_end(args);
+	return (len);
+}
+
---
+++ main.h
@@ -0,0 +1,51 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @ch: type char pointer of the specifier
+ * @fh: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *ch;
+	int (*fh)();
+} convert_match;
+
+int _printf(const char *format, ...);
+int printf_int(va_list args);
+int printf_string(va_list val);
+int printf_char(va_list val);
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_percentage(void);
+int _putchar(char c);
+
+
+#endif
+
