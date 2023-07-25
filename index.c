db8c806aa07c499396a86459b0cb5c7c362d8195
AnyumbaKE <esanyumba@gmail.com>
2023-07-25 12:54:03 -0700

    index

--- index.c
+++ index.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * init_params - clears struct fields and reset buf
+ * init_params - clears struct fields and reset buffer.
  * @params: the parameters struct
  * @ap: the argument pointer
  * Return: void
